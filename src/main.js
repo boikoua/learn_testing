32ff4ac00f6eda8d37b1f9d10afcbdbb343ba374
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-04-09 16:55:10 +0300

    start learn testing on JS

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import './assets/styles/index.scss';
-
-createApp(App).mount('#app');
